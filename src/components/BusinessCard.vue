abf0c559b3f34e4216cae4fa71c85924f68991f1
644792799@qq.com <644792799@qq.com>
2017-01-23 16:54:33 +0800

    --

--- src/components/BusinessCard.vue
+++ src/components/BusinessCard.vue
@@ -1,9 +1,19 @@
 <template>
-	<div id="businessCard">
-		<div class="userPhoto">
-			<img :src="imgurl == undefined || imgurl == null || imgurl == '' ? defaultimgurl : imgurl">
+	<div class="card-user">
+		<div class="card-avatar">
+			<img :src="userInfo.avatar == undefined || userInfo.avatar == null || userInfo.avatar == '' ? defaultimgurl : userInfo.avatar">
 		</div>
-		<div class="socialNetwork"></div>
+		<div class="card-names">
+			<span class="card-fullname d-block"><b>{{userInfo.fullname}}</b></span>
+			<span class="card-username d-block">{{userInfo.username}}</span>
+		</div>
+		<div class="card-socialnetwork">
+			<ul>
+				<li v-for="nw in userInfo.socialnetwork">
+					<img :src="nw.imgurl">
+				</li>
+			</ul>
+		</div>
 	</div>
 </template>
 
@@ -14,13 +24,60 @@
 				defaultimgurl: '/static/img/头像示例.jpg'
 			}
 		},
-		props: ['imgurl']
+		props: ['userInfo']
 	}
 </script>
 
 <style type="text/css">
-	.userPhoto img{
-		width: 100px;
-		height: 100px;
+	.card-user{
+		width: 95%;
+		height: auto;
+		margin: 5px auto 5px auto;
+		background: white;
+		border: 1px solid #eee;
+    	padding: 10px;
+	}
+	.card-avatar img{
+		width: 100%;
+		height: auto;
+	}
+	.card-socialnetwork{
+		height: 30px;
+	}
+	.card-socialnetwork ul{
+		list-style: none;
+		padding: 0;
+	}
+	.card-socialnetwork ul li{
+		float: left;
+		margin-right: 5px;
+	}
+	.card-socialnetwork ul li img{
+		width: 20px;
+		height: 20px;
+	}
+	.card-socialnetwork ul li img:hover{
+		*opacity: 20;
+		border: 1px solid #eee;
+	}
+	.card-names{
+		margin-top: 10px;
+	}
+	.card-names .card-username{
+		font-size: 20px;
+	    font-style: normal;
+	    font-weight: 300;
+	    line-height: 24px;
+	    color: #666;
+	    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+	}
+	.card-names .card-fullname{
+		font-size: 20px;
+	}
+	.d-block{
+		display: block !important;
+	}
+	.panel-body {
+	    padding: 0!important;
 	}
 </style>
--- src/components/firstComponent.vue
+++ src/components/firstComponent.vue
@@ -1,8 +1,16 @@
 <template>
-	<div>
-		<h1>组件一</h1>
-		<a href="#">written by {{author}}</a>
-		<businessCard :imgurl="userImg"></businessCard>
+	<div class="row">
+		<div class="col-md-4">
+			<div class="panel panel-default">
+			  <div class="panel-heading">用户卡片</div>
+			  <div class="panel-body">
+			    <businessCard :userInfo="userInfo"></businessCard>
+			  </div>
+			  <div class="panel-footer text-right">作者:{{author}}</div>
+			</div>
+		</div>
+		<div class="col-md-4"></div>
+		<div class="col-md-4"></div>
 	</div>
 </template>
 
@@ -12,7 +20,24 @@
 		data(){
 			return {
 				author: "cjay",
-				userImg: '/static/img/头像示例2.jpg'
+				userInfo: {
+					username: '644792799',
+					fullname: 'CJay',
+					avatar: '/static/img/头像示例2.jpg',
+					socialnetwork: [{
+						imgurl: '/static/img/qq.png',
+						name: 'qq'
+					}, {
+						imgurl: '/static/img/github.png',
+						name: 'github'
+					}, {
+						imgurl: '/static/img/weibo.png',
+						name: 'weibo'
+					}, {
+						imgurl: '/static/img/google.png',
+						name: 'google'
+					}]
+				}
 			}
 		},
 		components: {
