32f8bafde4c95874e1b5ada877d2fc912e6c9741
644792799 <644792799@qq.com>
2017-07-26 02:11:12 +0800

    --

--- src/components/sms/markdown/src/main.vue
+++ src/components/sms/markdown/src/main.vue
@@ -12,8 +12,10 @@
     <div class="sms-markdown-tools" id="sms-markdown-tools">
       <span class="icon-link2"></span>
       <span class="icon-mention"></span>
-      <span class="icon-quote"></span>
+      <span class="icon-quote" @click="quote"></span>
       <span class="icon-embed2" @click="code"></span>
+      <span class="icon-list-ul" @click="listul"></span>
+      <span class="icon-list-ol" @click="listol"></span>
       <span class="icon-question"></span>
     </div>
   </div>
@@ -46,21 +48,21 @@
         preMark: exText,
         activeName: 'edit',
         postMark: '',
-        editor: undefined
+        cm: undefined
       }
     },
     mounted(){
         var self = this;
         var myTextarea = document.getElementById("txtarea");
-         this.editor = CodeMirror.fromTextArea(myTextarea, {
+         this.cm = CodeMirror.fromTextArea(myTextarea, {
             mode: 'gfm',
             lineNumbers: false,
             theme: "paper",
             lineWrapping: true,
             scrollbarStyle: "simple"
           });
-         this.editor.on("change", function(instance,changeObj){
-            var editorVal = self.editor.getValue();
+         this.cm.on("change", function(instance,changeObj){
+            var editorVal = self.cm.getValue();
             self.preMark = editorVal;
          });
     },
@@ -102,10 +104,397 @@
           elem.className = newClass.replace(/^\s+|\s+$/g, '');
         }
       },
-      code(e){
-        //e.preventDefault();
-        var selectstr = this.editor.getSelection();
-        this.editor.replaceSelection("`" + selectstr + "`");
+      // code(e){
+      //   //e.preventDefault();
+      //   var selectstr = this.editor.getSelection();
+      //   this.editor.replaceSelection("`" + selectstr + "`");
+      // },
+      // quote(){
+      //   var cm        = this.editor;
+      //   var cursor    = cm.getCursor();
+      //   var selection = cm.getSelection();
+
+      //   if (cursor.ch !== 0)
+      //   {
+      //       cm.setCursor(cursor.line, 0);
+      //       cm.replaceSelection("> " + selection);
+      //       cm.setCursor(cursor.line, cursor.ch + 2);
+      //   }
+      //   else
+      //   {
+      //       cm.replaceSelection("> " + selection);
+      //   }
+      // },
+      undo : function() {
+          this.cm.undo();
+      },
+      
+      redo : function() {
+          this.cm.redo();
+      },
+      
+      bold : function() {
+          var cm        = this.cm;
+          var cursor    = cm.getCursor();
+          var selection = cm.getSelection();
+
+          cm.replaceSelection("**" + selection + "**");
+
+          if(selection === "") {
+              cm.setCursor(cursor.line, cursor.ch + 2);
+          }
+      },
+      
+      del : function() {
+          var cm        = this.cm;
+          var cursor    = cm.getCursor();
+          var selection = cm.getSelection();
+
+          cm.replaceSelection("~~" + selection + "~~");
+
+          if(selection === "") {
+              cm.setCursor(cursor.line, cursor.ch + 2);
+          }
+      },
+
+      italic : function() {
+          var cm        = this.cm;
+          var cursor    = cm.getCursor();
+          var selection = cm.getSelection();
+
+          cm.replaceSelection("*" + selection + "*");
+
+          if(selection === "") {
+              cm.setCursor(cursor.line, cursor.ch + 1);
+          }
+      },
+
+      quote : function() {
+          var cm        = this.cm;
+          var cursor    = cm.getCursor();
+          var selection = cm.getSelection();
+
+          if (cursor.ch !== 0)
+          {
+              cm.setCursor(cursor.line, 0);
+              cm.replaceSelection("> " + selection);
+              cm.setCursor(cursor.line, cursor.ch + 2);
+          }
+          else
+          {
+              cm.replaceSelection("> " + selection);
+          }
+
+          //cm.replaceSelection("> " + selection);
+          //cm.setCursor(cursor.line, (selection === "") ? cursor.ch + 2 : cursor.ch + selection.length + 2);
+      },
+      
+      ucfirst : function() {
+          var cm         = this.cm;
+          var selection  = cm.getSelection();
+          var selections = cm.listSelections();
+
+          cm.replaceSelection(editormd.firstUpperCase(selection));
+          cm.setSelections(selections);
+      },
+      
+      ucwords : function() {
+          var cm         = this.cm;
+          var selection  = cm.getSelection();
+          var selections = cm.listSelections();
+
+          cm.replaceSelection(editormd.wordsFirstUpperCase(selection));
+          cm.setSelections(selections);
+      },
+      
+      uppercase : function() {
+          var cm         = this.cm;
+          var selection  = cm.getSelection();
+          var selections = cm.listSelections();
+
+          cm.replaceSelection(selection.toUpperCase());
+          cm.setSelections(selections);
+      },
+      
+      lowercase : function() {
+          var cm         = this.cm;
+          var cursor     = cm.getCursor();
+          var selection  = cm.getSelection();
+          var selections = cm.listSelections();
+          
+          cm.replaceSelection(selection.toLowerCase());
+          cm.setSelections(selections);
+      },
+
+      h1 : function() {
+          var cm        = this.cm;
+          var cursor    = cm.getCursor();
+          var selection = cm.getSelection();
+
+          if (cursor.ch !== 0)
+          {
+              cm.setCursor(cursor.line, 0);
+              cm.replaceSelection("# " + selection);
+              cm.setCursor(cursor.line, cursor.ch + 2);
+          }
+          else
+          {
+              cm.replaceSelection("# " + selection);
+          }
+      },
+
+      h2 : function() {
+          var cm        = this.cm;
+          var cursor    = cm.getCursor();
+          var selection = cm.getSelection();
+
+          if (cursor.ch !== 0)
+          {
+              cm.setCursor(cursor.line, 0);
+              cm.replaceSelection("## " + selection);
+              cm.setCursor(cursor.line, cursor.ch + 3);
+          }
+          else
+          {
+              cm.replaceSelection("## " + selection);
+          }
+      },
+
+      h3 : function() {
+          var cm        = this.cm;
+          var cursor    = cm.getCursor();
+          var selection = cm.getSelection();
+
+          if (cursor.ch !== 0)
+          {
+              cm.setCursor(cursor.line, 0);
+              cm.replaceSelection("### " + selection);
+              cm.setCursor(cursor.line, cursor.ch + 4);
+          }
+          else
+          {
+              cm.replaceSelection("### " + selection);
+          }
+      },
+
+      h4 : function() {
+          var cm        = this.cm;
+          var cursor    = cm.getCursor();
+          var selection = cm.getSelection();
+
+          if (cursor.ch !== 0)
+          {
+              cm.setCursor(cursor.line, 0);
+              cm.replaceSelection("#### " + selection);
+              cm.setCursor(cursor.line, cursor.ch + 5);
+          }
+          else
+          {
+              cm.replaceSelection("#### " + selection);
+          }
+      },
+
+      h5 : function() {
+          var cm        = this.cm;
+          var cursor    = cm.getCursor();
+          var selection = cm.getSelection();
+
+          if (cursor.ch !== 0)
+          {
+              cm.setCursor(cursor.line, 0);
+              cm.replaceSelection("##### " + selection);
+              cm.setCursor(cursor.line, cursor.ch + 6);
+          }
+          else
+          {
+              cm.replaceSelection("##### " + selection);
+          }
+      },
+
+      h6 : function() {
+          var cm        = this.cm;
+          var cursor    = cm.getCursor();
+          var selection = cm.getSelection();
+
+          if (cursor.ch !== 0)
+          {
+              cm.setCursor(cursor.line, 0);
+              cm.replaceSelection("###### " + selection);
+              cm.setCursor(cursor.line, cursor.ch + 7);
+          }
+          else
+          {
+              cm.replaceSelection("###### " + selection);
+          }
+      },
+
+      "listul" : function() {
+          var cm        = this.cm;
+          var cursor    = cm.getCursor();
+          var selection = cm.getSelection();
+
+          if (selection === "") 
+          {
+              cm.replaceSelection("- " + selection);
+          } 
+          else 
+          {
+              var selectionText = selection.split("\n");
+
+              for (var i = 0, len = selectionText.length; i < len; i++) 
+              {
+                  selectionText[i] = (selectionText[i] === "") ? "" : "- " + selectionText[i];
+              }
+
+              cm.replaceSelection(selectionText.join("\n"));
+          }
+      },
+
+      "listol" : function() {
+          var cm        = this.cm;
+          var cursor    = cm.getCursor();
+          var selection = cm.getSelection();
+
+          if(selection === "") 
+          {
+              cm.replaceSelection("1. " + selection);
+          }
+          else
+          {
+              var selectionText = selection.split("\n");
+
+              for (var i = 0, len = selectionText.length; i < len; i++) 
+              {
+                  selectionText[i] = (selectionText[i] === "") ? "" : (i+1) + ". " + selectionText[i];
+              }
+
+              cm.replaceSelection(selectionText.join("\n"));
+          }
+      },
+
+      hr : function() {
+          var cm        = this.cm;
+          var cursor    = cm.getCursor();
+          var selection = cm.getSelection();
+
+          cm.replaceSelection(((cursor.ch !== 0) ? "\n\n" : "\n") + "------------\n\n");
+      },
+
+      tex : function() {
+          // if (!this.settings.tex)
+          // {
+          //     alert("settings.tex === false");
+          //     return this;
+          // }
+          
+          var cm        = this.cm;
+          var cursor    = cm.getCursor();
+          var selection = cm.getSelection();
+
+          cm.replaceSelection("$$" + selection + "$$");
+
+          if(selection === "") {
+              cm.setCursor(cursor.line, cursor.ch + 2);
+          }
+      },
+
+      link : function() {
+          // this.executePlugin("linkDialog", "link-dialog/link-dialog");
+      },
+
+      "reference-link" : function() {
+          // this.executePlugin("referenceLinkDialog", "reference-link-dialog/reference-link-dialog");
+      },
+
+      pagebreak : function() {
+          if (!this.settings.pageBreak)
+          {
+              alert("settings.pageBreak === false");
+              return this;
+          }
+          
+          var cm        = this.cm;
+          var selection = cm.getSelection();
+
+          cm.replaceSelection("\r\n[========]\r\n");
+      },
+
+      image : function() {
+          // this.executePlugin("imageDialog", "image-dialog/image-dialog");
+      },
+      
+      code : function() {
+          var cm        = this.cm;
+          var cursor    = cm.getCursor();
+          var selection = cm.getSelection();
+
+          cm.replaceSelection("`" + selection + "`");
+
+          if (selection === "") {
+              cm.setCursor(cursor.line, cursor.ch + 1);
+          }
+      },
+
+      "code-block" : function() {
+          // this.executePlugin("codeBlockDialog", "code-block-dialog/code-block-dialog");            
+      },
+
+      "preformatted-text" : function() {
+          // this.executePlugin("preformattedTextDialog", "preformatted-text-dialog/preformatted-text-dialog");
+      },
+      
+      table : function() {
+          // this.executePlugin("tableDialog", "table-dialog/table-dialog");         
+      },
+      
+      datetime : function() {
+          var cm        = this.cm;
+          var selection = cm.getSelection();
+          var date      = new Date();
+          var langName  = this.settings.lang.name;
+          var datefmt   = editormd.dateFormat() + " " + editormd.dateFormat((langName === "zh-cn" || langName === "zh-tw") ? "cn-week-day" : "week-day");
+
+          cm.replaceSelection(datefmt);
+      },
+      
+      emoji : function() {
+          // this.executePlugin("emojiDialog", "emoji-dialog/emoji-dialog");
+      },
+              
+      "html-entities" : function() {
+          // this.executePlugin("htmlEntitiesDialog", "html-entities-dialog/html-entities-dialog");
+      },
+              
+      "goto-line" : function() {
+          // this.executePlugin("gotoLineDialog", "goto-line-dialog/goto-line-dialog");
+      },
+
+      watch : function() {    
+          // this[this.settings.watch ? "unwatch" : "watch"]();
+      },
+
+      preview : function() {
+          // this.previewing();
+      },
+
+      fullscreen : function() {
+          // this.fullscreen();
+      },
+
+      clear : function() {
+          // this.clear();
+      },
+      
+      search : function() {
+          // this.search();
+      },
+
+      help : function() {
+          // this.executePlugin("helpDialog", "help-dialog/help-dialog");
+      },
+
+      info : function() {
+          // this.showInfoDialog();
       }
     }
   };
