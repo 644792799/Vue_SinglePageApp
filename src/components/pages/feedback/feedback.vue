2f3c8b3e542315c4b22e00c6b1d86471489bbe1b
644792799 <644792799@qq.com>
2018-01-17 16:53:35 +0800

    --

--- src/components/pages/feedback/feedback.vue
+++ src/components/pages/feedback/feedback.vue
@@ -5,10 +5,31 @@
 				<span style="line-height:56px;font-size:18px;color:#586069;"><i class="icon-terminal" style="font-weight:600;"></i>&nbsp;意见&建议</span>
 				<SmsLine gradfrom="left"></SmsLine>
 			</div>
-			<div>
+			<div class="qacontainer">
 				<ul>
-					<li></li>
-					<li></li>
+					<li>
+						<div>
+							<span>
+								<a href="">
+									<i class="icon-bug"></i>
+									8 Bug
+								</a>
+							</span>
+							<span>
+								<a href="">
+									<i class="icon-issue-opened"></i>
+									8 意见
+								</a>
+							</span>
+						</div>
+					</li>
+					<li>
+						<div style="width:50px;">111</div>
+						<div style="flex:1">
+							<div>222</div>
+							<div>333</div>
+						</div>
+					</li>
 				</ul>
 			</div>
 		</div>
@@ -18,6 +39,9 @@
 	
 </script>
 <style type="text/css">
+	.feedback{
+		*font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+	}
 	.feedback>div{
 		padding: 0 50px;
 		margin-top: 15px;
@@ -26,7 +50,46 @@
 
 	.feedback .filtercontainer{
 		position: relative;
-		
 		height: 56px;
+	}
+
+	.feedback .qacontainer{
+		box-shadow: 0 0 8px 0 rgba(232,237,250,.6), 0 2px 4px 0 rgba(232,237,250,.5);
+	}
+
+	.feedback .qacontainer ul{
+		padding: 0;
+    	list-style: none;
+    	font-size: 14px;
+	}
+
+	.feedback .qacontainer ul li{
+		height: 55px;
+	}
+
+	.feedback .qacontainer ul li:first-child{
+		background: var(--banner-color, #eef1f6);
+		height: 49px;
+		display: flex;
+		align-items: center;
+		padding-left: 15px;
+	}
+
+	.feedback .qacontainer ul li+li{
+		border-top: 1px solid var(--border-color, #e4e8f1);
+		display: flex;
+		flex-direction: row;
+	}
+
+	.feedback .qacontainer ul li:first-child a{
+		color: var(--sub-title-color, #4E5359);
+	}
+
+	.feedback .qacontainer ul li:first-child a:hover {
+	    color: var(--title-color, #20a0ff)!important;
+	}
+
+	.feedback .qacontainer ul li span+span{
+		margin-left: 10px;
 	}
 </style>
